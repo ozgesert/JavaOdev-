dab5fc7757ce9dc8bbd1dfdae51b33c89d0a61f4
ozgesert <ozgesert@yahoo.com>
2022-09-25 17:29:46 +0300

    CalculatorDemo

---
+++ ReCapDemo_Classes/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3, 5);
+		System.out.println(sonuc);
+		int sonuc2 = dortIslem.Cikar(6, 1);
+		System.out.println(sonuc2);
+
+	}
+
+}
