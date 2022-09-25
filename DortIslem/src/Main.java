b1f54e91323b3f80ab924affe312dd06722f5b92
ozgesert <ozgesert@yahoo.com>
2022-09-25 20:05:25 +0300

    Update

--- DortIslem/src/Main.java
+++ DortIslem/src/Main.java
@@ -2,9 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		DortIslem dortIslem = new DortIslem();
-		dortIslem.topla(5, 2);
-		
+		DortIslem dortIslem = new DortIslem();	
+		System.out.println(dortIslem.topla(5, 2));
 
 	}
 
